c8ce14507e33027601f48c3996efaa4d553600e0
Barraza44 <diegobarrazapascual@hotmail.com>
2020-10-20 22:39:08 -0500

    added DOM class

---
+++ src/dom.ts
@@ -0,0 +1,10 @@
+class DOM{
+  public static showProjectTab() {
+    const projectTab = document.querySelector(".projects");
+    projectTab.setAttribute("class", "projects-shown");
+  }
+}
+
+
+
+export { DOM };
