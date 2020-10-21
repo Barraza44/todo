32aee497d260e06817c269cc2ee9cdf3a7fd16df
Barraza44 <diegobarrazapascual@hotmail.com>
2020-10-21 18:06:03 -0500

    added create todo method

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ const main = document.querySelector("main");
 const addIcon = document.querySelector(".add-proj");
 const menuIcon = document.querySelector(".menu");
 const cancelButton = document.querySelector("#cancel");
+const addButton = document.querySelector("#add");
 
 menuIcon.addEventListener("click", () => {
   DOM.showProjectTab();
@@ -24,5 +25,23 @@ addIcon.addEventListener("click", () => {
 cancelButton.addEventListener("click", () => {
   DOM.hideFormContainer();
 });
+
+let todoList = [];
+
+function CreateTodo(){
+  const title = document.querySelector("#todo-title");
+  const description = document.querySelector("#todo-description");
+  const priority = document.querySelector("#todo-priority");
+  const dueDate = document.querySelector("#todo-date");
+
+  // @ts-ignore
+  let myTodo = new ToDo(title.value, description.value, priority.value, dueDate.value);
+  todoList.push(myTodo);
+  myTodo.index = todoList.indexOf(myTodo);
+  DOM.renderCard(myTodo);
+  DOM.hideFormContainer();
+}
+
+addButton.addEventListener("click", CreateTodo);
 
 export { addIcon, menuIcon, main, body}
