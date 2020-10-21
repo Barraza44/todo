89e41b732dd8b930a910ec3198eeedc50c70e621
Barraza44 <diegobarrazapascual@hotmail.com>
2020-10-20 22:39:23 -0500

    added icon selectors

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,12 @@
 import "./style.css";
+import { DOM } from "./dom";
 
+const main = document.querySelector("main");
 const addIcon = document.querySelector(".add-proj");
 const menuIcon = document.querySelector(".menu");
+
+menuIcon.addEventListener("click", () =>{
+  DOM.showProjectTab();
+})
+
+export { addIcon, menuIcon, main }
