696aba0e0c56d9cd2dc8286b46fcec347ebc7f25
Barraza44 <diegobarrazapascual@hotmail.com>
2020-10-21 17:35:56 -0500

    added index property

--- src/to-doClass.ts
+++ src/to-doClass.ts
@@ -3,12 +3,14 @@ class ToDo {
   public title: string;
   public description: string;
   public dueDate: string;
+  public index: number;
 
   constructor(title: string, description: string, priority: string, dueDate: string) {
     this.title = title;
     this.description = description;
     this.priority = priority;
     this.dueDate = dueDate;
+    this.index = 0;
   }
 }
 
