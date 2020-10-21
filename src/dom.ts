5f2f1281d7ae080fbae509c20d31d951c692759a
Barraza44 <diegobarrazapascual@hotmail.com>
2020-10-21 17:19:17 -0500

    added DOM.renderCard() method

--- src/dom.ts
+++ src/dom.ts
@@ -1,8 +1,11 @@
-import {main} from "./index";
+import {main, body} from "./index";
+import {ToDo} from "./to-doClass";
 
-const body = document.querySelector("body");
+
 const projectTab = document.querySelector(".projects");
 const formContainer = document.querySelector(".form-container");
+const calendar = document.querySelector(".calendar");
+const remove = document.querySelector(".delete");
 
 
 class DOM {
@@ -24,6 +27,37 @@ class DOM {
   public static hideFormContainer() {
     formContainer.setAttribute("class", "form-container");
     body.classList.remove("body-h");
+  }
+
+  public static renderCard(todo: ToDo) {
+    const cardContainer = document.createElement("div");
+    main.appendChild(cardContainer);
+    cardContainer.setAttribute("class", "card");
+
+    const cardTitle = document.createElement("h2");
+    cardTitle.textContent = todo.title;
+    cardContainer.appendChild(cardTitle);
+
+    const cardText = document.createElement("p")
+    cardText.setAttribute("class", "card-text");
+    cardText.textContent = todo.description;
+    cardContainer.appendChild(cardText);
+
+    const dateContainer = document.createElement("div");
+    cardContainer.appendChild(dateContainer);
+    const dateIcon = calendar.cloneNode(true);
+    dateContainer.appendChild(dateIcon);
+    const dateText = document.createElement("p");
+    dateText.textContent = todo.dueDate;
+    dateContainer.appendChild(dateText);
+
+    const seeMoreBtn = document.createElement("button");
+    seeMoreBtn.setAttribute("class", "see");
+    cardContainer.appendChild(seeMoreBtn);
+
+    const deleteBtn = remove.cloneNode(true);
+    cardContainer.appendChild(deleteBtn);
+
   }
 }
 
