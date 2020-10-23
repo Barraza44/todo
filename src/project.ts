255ed4a45c42b8cec2a01e1792e9d7dc1843278c
Barraza44 <diegobarrazapascual@hotmail.com>
2020-10-23 16:45:37 -0500

    added createTodo method on project class

--- src/project.ts
+++ src/project.ts
@@ -1,9 +1,27 @@
 import { ToDo } from "./to-doClass";
 import { DOM } from "./dom";
 
-class Project{
+class Project {
   public name: string;
+  public todos: object[];
   constructor(name: string) {
     this.name = name;
+    this.todos = [];
+  }
+  public createTodo() {
+    const title = document.querySelector("#todo-title");
+    const description = document.querySelector("#todo-description");
+    const priority = document.querySelector("#todo-priority");
+    const dueDate = document.querySelector("#todo-date");
+
+    // @ts-ignore
+    let myTodo = new ToDo(title.value, description.value, priority.value, dueDate.value);
+    this.todos.push(myTodo);
+    myTodo.index = this.todos.indexOf(myTodo);
+    DOM.renderCard(myTodo);
+    DOM.hideFormContainer();
   }
 }
+
+
+export default Project;
